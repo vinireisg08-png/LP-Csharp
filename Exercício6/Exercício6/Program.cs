17700f04759cc7d6c1ebe355c9e301ab03608205
vinireisg08-png <vini.reisg08@gmail.com>
2026-05-25 11:42:16 +0100

    Exercício6

---
+++ Exercício6/Exercício6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class Exercício6
+{
+    public static void Main(string[] args)
+    {
+        int diasemana = 2;
+        switch (diasemana)
+        {
+            case 1:
+                Console.WriteLine("Segunda-Feira");
+                break;
+
+            case 2:
+                Console.WriteLine("Terça-Feira");
+                break;
+
+            case 3:
+                Console.WriteLine("Quarta-Feira");
+                break;
+
+            case 4:
+                Console.WriteLine("Quinta-Feira");
+                break;
+
+            case 5:
+                Console.WriteLine("Sexta-Feira");
+                break;
+
+            case 6:
+                Console.WriteLine("Sábado");
+                break;
+
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+
+        }
+    }
+}
+                
+                
