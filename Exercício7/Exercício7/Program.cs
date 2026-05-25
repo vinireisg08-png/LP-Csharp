62008398036a819e1f1a490cb0d53b02c50adc54
vinireisg08-png <vini.reisg08@gmail.com>
2026-05-25 12:54:46 +0100

    Exercício7

---
+++ Exercício7/Exercício7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Exercício6
+{
+    public static void Main(string[] args)
+    {
+        int nota = 100 ;
+        switch (nota)
+        {
+            case int n when (nota >= 90):
+                Console.WriteLine("Nota: A");
+                break;
+            case int n when (nota >= 80):
+                Console.WriteLine("Nota: B");
+                break;
+            case int n when (nota >= 70):
+                Console.WriteLine("Nota: C");
+                break;
+            case int n when (nota >= 60):
+                Console.WriteLine("Nota: D");
+                break;
+            case int n when (nota < 60):
+                Console.WriteLine("Reprovado");
+                break;
+            default:
+                Console.WriteLine("Não existe nota");
+                break;
+
+
+        }
+    }
+}
