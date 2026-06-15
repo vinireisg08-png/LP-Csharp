17ca1f880e996f430038885b072d46f6f7ad5bbc
vinireisg08-png <vini.reisg08@gmail.com>
2026-06-15 11:58:17 +0100

    Exercício 9

---
+++ Exercíicio9/Exercíicio9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+public class Exercício9
+{
+    public static void Main(string[] args)
+    {
+        for (int counter = 1; counter <= 20; counter++)
+        {
+            if(counter %2 == 0)
+            {
+                Console.WriteLine(counter);
+            }
+            
+        }
+    }
+}
