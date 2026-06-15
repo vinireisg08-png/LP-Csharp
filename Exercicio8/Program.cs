d82285a9b0c1a6091ba1dca97cb64719f3606c51
vinireisg08-png <vini.reisg08@gmail.com>
2026-06-15 10:21:30 +0100

    Vinicius e Andrii

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Exercício8
+{
+    public static void Main(string[] args)
+    {
+        int nota = 12;
+        switch (nota)
+        {
+            case int n when (nota > 20 || nota < 0):
+                Console.WriteLine("Nota inválida");
+                break;
+            case int n when (nota >= 17):
+                Console.WriteLine("Excelente");
+                break;
+            case int n when (nota >= 14):
+                Console.WriteLine("Boa");
+                break;
+            case int n when (nota >=10):
+                Console.WriteLine("Suficiente");
+                break;
+            case int n when (nota <=9):
+                Console.WriteLine("Insuficiente");
+                break;
+            default:
+                Console.WriteLine("Nota inválida");
+                break;
+
+
+        }
+    }
+}
