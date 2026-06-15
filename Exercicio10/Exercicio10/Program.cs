a135dd1b85b2b2ceee36f335e6b8724c6b0aabe6
vinireisg08-png <vini.reisg08@gmail.com>
2026-06-15 13:01:05 +0100

    Exercício10

---
+++ Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.PerformanceData;
+using System.Reflection;
+public class Exercício10
+{
+    public static void Main(string[] args)
+    {
+        int soma = 0;
+        for (int counter = 1; counter <= 50; counter++)
+        {
+            if (counter % 2 != 0)
+            {
+                soma += counter;
+            }
+           
+        }
+        Console.WriteLine(soma);
+    }
+}
