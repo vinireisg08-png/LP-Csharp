beae787a5b15792c3d24933d4c46c18ad49f1449
vinireisg08-png <vini.reisg08@gmail.com>
2026-06-22 11:30:01 +0100

    Exercício12

---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public class Exercício12
+{
+
+    public static void Main(string[] args)
+    {
+        int soma = 0;
+        int counter;
+
+        for (counter = 0; soma <= 100; counter++)
+        {
+            soma += counter;
+            Console.WriteLine(soma);
+        }
+
+        Console.WriteLine("ultimo número adicionado: " + counter);
+    }
+}
